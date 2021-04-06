dfa994c63f48468b2b66cac5be36aa79dccf26ca
Maddie <maddiewaldie@me.com>
2021-04-06 11:21:41 -0700

    finished lab 2

--- project2/sorted.c
+++ project2/sorted.c
@@ -13,145 +13,145 @@
 # define MAX_SIZE 18000
 
 typedef struct set {
-    int count; // num elements
-    int length; // length of array
-    char **elts; // array
+	int count; // num elements
+	int length; // length of array
+	char **elts; // array
 } SET;
 
 /*
-    search: private function that uses binary search to go through array for elt
-    runtime: O(log(n))
+search: private function that uses binary search to go through array for elt
+runtime: O(log(n))
 */
 int search(SET *sp, char *elt, bool *found) {
-    assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
-    
-    int low = 0, high = sp->count-1, mid; // create vars for binary search
+	assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
 
-    while(low <=high) {
-        mid = (high+low)/2; // find midpoint of array
+	int low = 0, high = sp->count-1, mid; // create vars for binary search
 
-        if(strcmp(sp->elts[mid], elt) > 0) { // we know that elt is in first half of array
-            high = mid - 1;
-        }
-        else if(strcmp(sp->elts[mid], elt) < 0) { // we know elt is in second half of array
-            low = mid+1;
-        }
-        else { // we know this is elt
-            *found = true; 
-            return mid; // mid = index of elt
-        }
-    }
+	while(low <=high) {
+		mid = (high+low)/2; // find midpoint of array
 
-    *found = false;
-    return low;
+		if(strcmp(sp->elts[mid], elt) > 0) { // we know that elt is in first half of array
+			high = mid - 1;
+		}
+		else if(strcmp(sp->elts[mid], elt) < 0) { // we know elt is in second half of array
+			low = mid+1;
+		}
+		else { // we know this is elt
+			*found = true; 
+			return mid; // mid = index of elt
+		}
+	}
+
+	*found = false;
+	return low;
 }
 
 /*
-    createSet: return a pointer to a new set with a maximum capacity of maxElts
-    runtime: O(1)
+createSet: return a pointer to a new set with a maximum capacity of maxElts
+runtime: O(1)
 */
 SET *createSet(int maxElts) { 
-    SET*sp = malloc(sizeof(SET)); // declare and allocate memory for set
-    assert(sp != NULL); // check to make sure memory was allocated
+	SET*sp = malloc(sizeof(SET)); // declare and allocate memory for set
+	assert(sp != NULL); // check to make sure memory was allocated
 
-    sp->count = 0; // set count to number of elements currently in set (0 right now)
-    sp->length = maxElts; // set length to length of array
-    sp->elts = malloc(sizeof(char*)*maxElts); //allocate memory for array
-    assert(sp->elts != NULL); // check to make sure memory was allocated
+	sp->count = 0; // set count to number of elements currently in set (0 right now)
+	sp->length = maxElts; // set length to length of array
+	sp->elts = malloc(sizeof(char*)*maxElts); //allocate memory for array
+	assert(sp->elts != NULL); // check to make sure memory was allocated
 
-    return sp;
+	return sp;
 }
 
 /*
-    destroySet: deallocate memory associated with the set pointed to by sp
-    runtime: O(n)
+destroySet: deallocate memory associated with the set pointed to by sp
+runtime: O(n)
 */
 void destroySet(SET *sp) { 
-    int i;
-    for(i = 0; i < sp->count; i++) {
-        free(sp->elts[i]); // deallocate memory of elements in array
-    }
-    
-    free(sp->elts); // deallocate memory of the array
-    free(sp); //  deallocate memory of the set
+	int i;
+	for(i = 0; i < sp->count; i++) {
+		free(sp->elts[i]); // deallocate memory of elements in array
+	}
+
+	free(sp->elts); // deallocate memory of the array
+	free(sp); //  deallocate memory of the set
 }
 
 /*
-    numElements: return the number of elements in the set pointed to by sp
-    runtime: O(1)
+numElements: return the number of elements in the set pointed to by sp
+runtime: O(1)
 */
 int numElements(SET *sp) {
-    assert(sp != NULL); // make sure sp exists
-    return sp->count;
+	assert(sp != NULL); // make sure sp exists
+	return sp->count;
 }
 
 /*
-    addElement: add elt to the set pointed to by sp
-    runtime: O(nlog(n))
+addElement: add elt to the set pointed to by sp
+runtime: O(nlog(n))
 */
 void addElement(SET *sp, char *elt) { 
-    assert((sp != NULL) && (elt != NULL) && (sp->count < MAX_SIZE)); // make sure both sp & elt exist, and that count is less than max num of elements
-    
-    bool found;
-    int i = search(sp, elt, &found), j;
+	assert((sp != NULL) && (elt != NULL) && (sp->count < MAX_SIZE)); // make sure both sp & elt exist, and that count is less than max num of elements
 
-    if(found == false) { // only add elt if there isn't a duplicate of elt in sp
-        
-        for(j = sp -> count; j > i; j--) {
-            sp->elts[j] = sp->elts[j-1]; // shift everything to the right, freeing up a space for elt
-        }
+	bool found;
+	int i = search(sp, elt, &found), j;
 
-        sp->elts[i] = strdup(elt);
-        sp->count++; // increase count by 1
-    }
+	if(found == false) { // only add elt if there isn't a duplicate of elt in sp
+
+		for(j = sp -> count; j > i; j--) {
+			sp->elts[j] = sp->elts[j-1]; // shift everything to the right, freeing up a space for elt
+		}
+
+		sp->elts[i] = strdup(elt);
+		sp->count++; // increase count by 1
+	}
 }
 
 /*
-    removeElement: remove elt from the set pointed to by sp
-    runtime: O(nlog(n))
+removeElement: remove elt from the set pointed to by sp
+runtime: O(nlog(n))
 */
 void removeElement(SET *sp, char *elt) {
-    assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
+	assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
 
-    bool found;
-    int i = search(sp, elt, &found), j; // get index of elt
+	bool found;
+	int i = search(sp, elt, &found), j; // get index of elt
 
-    if(found) { // only remove elt if elt exists in the array
-        free(sp->elts[i]); // deallocate memory at index of elt
-        for(j = i; j < sp->count; j++) {
-            sp->elts[j] = sp->elts[j+1]; // shift everything to the left, filling up the empty space
-        }
+	if(found) { // only remove elt if elt exists in the array
+		free(sp->elts[i]); // deallocate memory at index of elt
+		for(j = i; j < sp->count; j++) {
+			sp->elts[j] = sp->elts[j+1]; // shift everything to the left, filling up the empty space
+		}
 
-        sp->count--; // decrease count by 1
-    }
+		sp->count--; // decrease count by 1
+	}
 }
 
 /*
-    findElement: if elt is present in the set pointed to by sp then return the matching element, otherwise return NULL
-    runtime: O(log(n))
+findElement: if elt is present in the set pointed to by sp then return the matching element, otherwise return NULL
+runtime: O(log(n))
 */
 char *findElement(SET *sp, char *elt) {
-    assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
-    
-    bool found;
-    search(sp, elt, &found);
-    return found ? elt : NULL; // if elt isn't present in set, return NULL, otherwise return elt
+	assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
+
+	bool found;
+	search(sp, elt, &found);
+	return found ? elt : NULL; // if elt isn't present in set, return NULL, otherwise return elt
 }
 
 /*
-    getElements: allocate and return an array of elements in the set pointed to by sp
-    runtime: O(n)
+getElements: allocate and return an array of elements in the set pointed to by sp
+runtime: O(n)
 */
 char **getElements(SET *sp) {
-    assert(sp != NULL); // make sure both sp exists
+	assert(sp != NULL); // make sure both sp exists
 
-    char **copy = malloc(sizeof(char*)*sp->count);
-    assert(copy != NULL); // check to make sure memory was allocated
-    
-    int i;
-    for(i = 0; i < sp->count; i ++) {
-        copy[i] = strdup(sp->elts[i]); // copy each elt into a copy array
-    }
+	char **copy = malloc(sizeof(char*)*sp->count);
+	assert(copy != NULL); // check to make sure memory was allocated
 
-    return copy;
+	int i;
+	for(i = 0; i < sp->count; i ++) {
+		copy[i] = strdup(sp->elts[i]); // copy each elt into a copy array
+	}
+
+	return copy;
 }
--- project2/unsorted.c
+++ project2/unsorted.c
@@ -12,120 +12,120 @@
 # define MAX_SIZE 18000
 
 typedef struct set {
-    int count; // num elements
-    int length; // length of array
-    char **elts; // array
+	int count; // num elements
+	int length; // length of array
+	char **elts; // array
 } SET;
 
 /*
-    search: private function that sequentially searches through array for elt; if element is not found, return -1.
-    runtime: O(n)
+search: private function that sequentially searches through array for elt; if element is not found, return -1.
+runtime: O(n)
 */ 
 int search(SET *sp, char *elt) {
-    assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
+	assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
 
-    int i;
-    for(i = 0; i < sp -> count; i++) {
-        if (strcmp(sp->elts[i],elt)==0) {
-            return i; // return index of elt
-        }
-    }
+	int i;
+	for(i = 0; i < sp -> count; i++) {
+		if (strcmp(sp->elts[i],elt)==0) {
+			return i; // return index of elt
+		}
+	}
 
-    return -1; // if element is not found return -1
+	return -1; // if element is not found return -1
 }
 
 /*
-    createSet: return a pointer to a new set with a maximum capacity of maxElts
-    runtime: O(1)
+createSet: return a pointer to a new set with a maximum capacity of maxElts
+runtime: O(1)
 */
 SET *createSet(int maxElts) { 
-    SET*sp = malloc(sizeof(SET)); // declare and allocate memory for set
-    assert(sp != NULL); // check to make sure memory was allocated
+	SET*sp = malloc(sizeof(SET)); // declare and allocate memory for set
+	assert(sp != NULL); // check to make sure memory was allocated
 
-    sp->count = 0; // set count to number of elements currently in set (0 right now)
-    sp->length = maxElts; // set length to length of array
-    sp->elts = malloc(sizeof(char*)*maxElts); //allocate memory for array
-    assert(sp->elts != NULL); // check to make sure memory was allocated
+	sp->count = 0; // set count to number of elements currently in set (0 right now)
+	sp->length = maxElts; // set length to length of array
+	sp->elts = malloc(sizeof(char*)*maxElts); //allocate memory for array
+	assert(sp->elts != NULL); // check to make sure memory was allocated
 
-    return sp;
+	return sp;
 }
 
 /*
-    destroySet: deallocate memory associated with the set pointed to by sp
-    runtime: O(n)
+destroySet: deallocate memory associated with the set pointed to by sp
+runtime: O(n)
 */
 void destroySet(SET *sp) { 
-    int i;
-    for(i = 0; i < sp->count; i++) {
-        free(sp->elts[i]); // deallocate memory of elements in array
-    }
-    
-    free(sp->elts); // deallocate memory of the array
-    free(sp); //  deallocate memory of the set
+	int i;
+	for(i = 0; i < sp->count; i++) {
+		free(sp->elts[i]); // deallocate memory of elements in array
+	}
+
+	free(sp->elts); // deallocate memory of the array
+	free(sp); //  deallocate memory of the set
 }
 
 /*
-    numElements: return the number of elements in the set pointed to by sp
-    runtime: O(1)
+numElements: return the number of elements in the set pointed to by sp
+runtime: O(1)
 */
 int numElements(SET *sp) {
-    assert(sp != NULL); // make sure sp exists
-    return sp->count;
+	assert(sp != NULL); // make sure sp exists
+	return sp->count;
 }
 
 /*
-    addElement: add elt to the set pointed to by sp
-    runtime: O(n)
+addElement: add elt to the set pointed to by sp
+runtime: O(n)
 */
 void addElement(SET *sp, char *elt) { 
-    assert((sp != NULL) && (elt != NULL) && (sp->count < MAX_SIZE)); // make sure both sp & elt exist, and that count is less than max num of elements
-    if(search(sp, elt) == -1) { // only add elt if there isn't a duplicate of elt in sp
-        sp->elts[sp -> count] = strdup(elt);
-        sp->count++;
-    }
+	assert((sp != NULL) && (elt != NULL) && (sp->count < MAX_SIZE)); // make sure both sp & elt exist, and that count is less than max num of elements
+	if(search(sp, elt) == -1) { // only add elt if there isn't a duplicate of elt in sp
+		sp->elts[sp -> count] = strdup(elt);
+		sp->count++;
+	}
 }
 
 /*
-    removeElement: remove elt from the set pointed to by sp
-    runtime: O(n)
+removeElement: remove elt from the set pointed to by sp
+runtime: O(n)
 */
 void removeElement(SET *sp, char *elt) {
-    assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
+	assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
 
-    int i = search(sp, elt); // get index of elt
+	int i = search(sp, elt); // get index of elt
 
-    if(i != -1) { // only remove elt if elt exists in the array
-        free(sp->elts[i]); // deallocate memory at index of elt
-        sp->elts[i] = sp->elts[sp->count - 1]; // put last element into the slot elt was previously in
-        sp->elts[sp->count - 1] = NULL; // set last element to null
-        sp->count--; // decrease count by 1
-    }
+	if(i != -1) { // only remove elt if elt exists in the array
+		free(sp->elts[i]); // deallocate memory at index of elt
+		sp->elts[i] = sp->elts[sp->count - 1]; // put last element into the slot elt was previously in
+		sp->elts[sp->count - 1] = NULL; // set last element to null
+		sp->count--; // decrease count by 1
+	}
 }
 
 /*
-    findElement: if elt is present in the set pointed to by sp then return the matching element, otherwise return NULL
-    runtime: O(n)
+findElement: if elt is present in the set pointed to by sp then return the matching element, otherwise return NULL
+runtime: O(n)
 */
 char *findElement(SET *sp, char *elt) {
-    assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
-    
-    return (search(sp, elt) == -1) ? NULL : elt; // if elt isn't present in set, return NULL, otherwise return elt
+	assert((sp != NULL) && (elt != NULL)); // make sure both sp & elt exist
+
+	return (search(sp, elt) == -1) ? NULL : elt; // if elt isn't present in set, return NULL, otherwise return elt
 }
 
 /*
-    getElements: allocate and return an array of elements in the set pointed to by sp
-    runtime: O(n)
+getElements: allocate and return an array of elements in the set pointed to by sp
+runtime: O(n)
 */
 char **getElements(SET *sp) {
-    assert(sp != NULL); // make sure sp exists
+	assert(sp != NULL); // make sure sp exists
 
-    char **copy = malloc(sizeof(char*)*sp->count);
-    assert(copy != NULL); // check to make sure memory was allocated
-    
-    int i;
-    for(i = 0; i < sp->count; i ++) {
-        copy[i] = strdup(sp->elts[i]); // copy each elt into a copy array
-    }
+	char **copy = malloc(sizeof(char*)*sp->count);
+	assert(copy != NULL); // check to make sure memory was allocated
 
-    return copy;
+	int i;
+	for(i = 0; i < sp->count; i ++) {
+		copy[i] = strdup(sp->elts[i]); // copy each elt into a copy array
+	}
+
+	return copy;
 }
