00ddcc40e5775fd2238ccb97bdf8ea847ea767a8
Maddie Waldie <maddiewaldie@me.com>
2021-05-04 11:17:46 -0400

    added empty functions to set

--- project4/set.c
+++ project4/set.c
@@ -2,3 +2,27 @@
  * Author: Madeleine Waldie
  * Due Date: 5/16/21
  */
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <assert.h>
+# include "set.h"
+# include <stdbool.h>
+# include "list.h"
+
+struct set{
+    int count;
+    int length;
+    LIST  **data;
+    int (*compare)();
+    unsigned (*hash)();
+};
+
+SET *createSet(int maxElts,int (*compare)(),unsigned (*hash)());
+void destroySet(SET *sp);
+int numElements(SET *sp);
+void addElement(SET *sp,void  *elt);
+void removeElement(SET *sp, void  *elt);
+void  *findElement(SET *sp, void *elt);
+void  *getElements(SET *sp);
